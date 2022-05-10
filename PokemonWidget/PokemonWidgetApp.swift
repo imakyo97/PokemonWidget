0adba65e5c4f172077b98f14596618c176785fea
kyohei.imamura <kyohei.imamura@p.lightcafe.co.jp>
2022-05-10 09:03:38 +0900

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by 今村京平 on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
